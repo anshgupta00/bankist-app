b08561c6a98c293e1b319cc9267522782a309561
Shyam Krishna Gupta <westudy430@gmail.com>
2024-06-28 14:44:03 +0545

    in-out-interest

--- bankist.js
+++ bankist.js
@@ -80,4 +80,36 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
+
 displayMovements(account1.movements);
+////////////////////////////////////////////////////////
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}$`;
+};
+calcDisplayBalance(account1.movements);
+
+///////////////////////////
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}$`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${out}$`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}$`;
+};
+calcDisplaySummary(account1.movements);
