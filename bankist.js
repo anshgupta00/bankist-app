8191d399b403f44fc78e4924a4bcf04c8f5c38f1
Shyam Krishna Gupta <westudy430@gmail.com>
2024-07-04 18:30:12 +0545

    internationalizing dates

--- bankist.js
+++ bankist.js
@@ -12,13 +12,13 @@ const account1 = {
     "2022-12-23T07:42:02.383Z",
     "2023-01-28T09:15:04.904Z",
     "2023-04-01T10:17:24.185Z",
-    "2023-05-08T14:11:59.604Z",
-    "2024-07-26T17:01:17.194Z",
-    "2024-07-28T23:36:17.929Z",
-    "2024-08-01T10:51:36.790Z",
+    "2024-06-30T14:11:59.604Z",
+    "2024-07-02T17:01:17.194Z",
+    "2024-07-03T23:36:17.929Z",
+    "2024-07-04T10:51:36.790Z",
   ],
-  // currency: "EUR",
-  // locale: "pt-PT", // de-DE
+  currency: "EUR",
+  locale: "en-US", // de-DE
 };
 
 const account2 = {
@@ -36,8 +36,8 @@ const account2 = {
     "2020-06-25T18:49:59.371Z",
     "2020-07-26T12:01:20.894Z",
   ],
-  // currency: "USD",
-  // locale: "en-US",
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -86,6 +86,33 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date, locale) {
+  // const date = new Date(acc.movementsDates[i]); // [i] that indexing helps to run loop for date as well, thus this a technique to run loop for two things
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Yesterday";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  // const day = `${date.getDate()}`.padStart(2, 0);
+  // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  // const year = date.getFullYear();
+  // return `${day}/${month}/${year}`;
+  return new Intl.DateTimeFormat(locale).format(date);
+};
+
+// const day = `${date.getDate()}`.padStart(2, 0);
+// const month = `${date.getMonth() + 1}`.padStart(2, 0);
+// const year = date.getFullYear();
+// return `${day}/${month}/${year}`;
+
+// const calcDaysPassed = (date1, date2) => {
+//   Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+// };
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
   const movs = sort
@@ -96,11 +123,11 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(acc.movementsDates[i]); // [i] that indexing helps to run loop for date as well, thus this a technique to run loop for two things
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
+    // const day = `${date.getDate()}`.padStart(2, 0);
+    // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    // const year = date.getFullYear();
 
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date, acc.locale); //(eventally we want to print userinterface)  //= `${day}/${month}/${year}`;
 
     const html = `
       <div class="movements__row">
@@ -204,16 +231,36 @@ btnLogin.addEventListener("click", function (e) {
     //create current date
 
     const now = new Date();
-    const day = `${now.getDate()}`.padStart(2, 0);
-    const month = `${now.getMonth() + 1}`.padStart(2, 0);
-    const year = now.getFullYear();
-    const hour = `${now.getHours()}`.padStart(2, 0);
-    const min = `${now.getMinutes()}`.padStart(2, 0);
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+    const options = {
+      hour: "numeric",
+      minute: "numeric",
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+      weekday: "long",
+    };
+    // const locale = navigator.language;
+    // console.log(locale);
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
+
+    // const now = new Date();
+    // const day = `${now.getDate()}`.padStart(2, 0);
+    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    // const year = now.getFullYear();
+    // const hour = `${now.getHours()}`.padStart(2, 0);
+    // const min = `${now.getMinutes()}`.padStart(2, 0);
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
+    ///////////////////////////////////////////////////////////////////
     //clear input fields
     inputLoginUsername.value = inputLoginPin.value = "";
     inputLoginPin.blur();
 
+    //////////////////////////////////////////////////////////////
     // displayMovements(currentAccount.movements);
     // calcDisplayBalance(currentAccount);
     // calcDisplaySummary(currentAccount);
@@ -304,3 +351,19 @@ btnSort.addEventListener("click", function (e) {
 //////////////////////////////////
 // Dates:
 console.log(new Date("December 24,2023"));
+
+//Experimenting with internationlizing dates
+
+// const now = new Date();
+// const options = {
+//   hour: "numeric",
+//   minute: "numeric",
+//   day: "numeric",
+//   month: "long",
+//   year: "numeric",
+//   weekday: "long",
+// };
+// const locale = navigator.language;
+// console.log(locale);
+
+// labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(now);
